f80919cdf00551b8a8d01840924645b65e8fd0ab
Claudio Romão <claudim_ucp@hotmail.com>
2017-02-10 11:48:41 -0200

    Fazendo apenas o download da imagem

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ ckage main
 import (
 	"io"
 	"os"
+	"log"
 
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/api/types"
@@ -11,35 +12,20 @@ import (
 )
 
 func main() {
+
+	logTo("init.out")
+	log.Println(" Create context")
 	ctx := context.Background()
+	log.Println(" Create NewEnvClient ")
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
-
-	_, err = cli.ImagePull(ctx, "alpine", types.ImagePullOptions{})
+	log.Println(" Client Created")
+	
+	_, err = cli.ImagePull(ctx, "nginx", types.ImagePullOptions{})
 	if err != nil {
-		panic(err)
-	}
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "alpine",
-		Cmd:   []string{"echo", "hello world"},
-	}, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
-
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
-	}
-
-	if _, err = cli.ContainerWait(ctx, resp.ID); err != nil {
-		panic(err)
-	}
-
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 
